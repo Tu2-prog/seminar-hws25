7c80303b653eeaae898705128df09fdf616ea7b4
Tu2-prog <anhtuduong.nguyen@outlook.de>
2025-09-29 21:21:46 +0200

    add functionality for adding logs into log dir

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from src.bot import Agent, Bot
 from src.model import ModelType
+from src.log_manager import LogManager
+
+log_manager = LogManager()
 
 if __name__ == "__main__":
     bot = Agent(Bot("Groq", ModelType.OPENAI))
     response = bot.respond("What is the capital of France?")
-    print(response)
+    log_manager.log("User: What is the capital of France?")
+    log_manager.log(f"Bot: {response}")
+    log_manager.log_to_file(
+        f"User: What is the capital of France?\nBot: {response}", "./logs/chat_log.csv"
+    )
--- src/log_manager.py
+++ src/log_manager.py
@@ -1,10 +1,17 @@
-from enum import Enum
+import os
+
 
 class LogManager:
-    """Central Log Manager for loggin the content of the LLM agents from JSON and put it into a csv file."""
+    """Central Log Manager for logging the content of the LLM agents."""
+
+    def __init__(self):
+        # Create a directory for logs if it doesn't exist
+        if not os.path.exists("logs"):
+            os.makedirs("logs")
+
     def log(self, message):
         print(message)
 
     def log_to_file(self, message, file_path):
-        with open(file_path, 'a') as file:
-            file.write(message + '\n')
+        with open(file_path, "a") as file:
+            file.write(message + "\n")
